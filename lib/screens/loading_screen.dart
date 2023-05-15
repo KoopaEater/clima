4677ed7bd013ab425438cf0651368e86d402d9bd
Max Kørner Andersen <maxkandersen@gmail.com>
2023-05-15 15:24:26 +0200

    More location stuff and somewhat API calls to open weather map.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
+import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -8,23 +9,42 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+    getData();
+  }
+
   void getLocation() async {
-    await Geolocator.requestPermission();
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
-    print(position);
+
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    print('Latitude: ${location.latitude ?? 'unknown'}');
+    print('Longitude: ${location.longitude ?? 'unknown'}');
+
+
+  }
+
+  void getData() async {
+
+    double lat = 56.4695993;
+    double lon = 10.0014371;
+
+    Uri apiCall = Uri.http('api.openweathermap.org', 'data/2.5/weather', {'lat': lat.toStringAsFixed(2), 'lon': lon.toStringAsFixed(2), 'appid': 'bcd5d3714b7ae41e0a5d5efbc5c21922'});
+    http.Response response = await http.get(apiCall);
+
+    if (response.statusCode == 200) {
+      print(response.body);
+    } else {
+      print(response.statusCode);
+    }
+
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
---
+++ lib/services/location.dart
@@ -0,0 +1,23 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+
+  double? latitude;
+  double? longitude;
+
+  Future<void> getCurrentLocation() async {
+
+    await Geolocator.requestPermission();
+
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.best);
+      latitude = position.latitude;
+      longitude = position.longitude;
+    } catch (e) {
+      print(e);
+    }
+
+  }
+
+}
